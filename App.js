9bf7251f2b896e989600257c4191aa37b7cd3e69
Mudassir4244 <147780881+Mudassir4244@users.noreply.github.com>
2025-05-14 22:49:29 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,36 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header >
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+          
+        >
+          Learn React
+        </a> */}
+        <h1>
+          Cloud Computing OEL
+        </h1>
+        <h1>
+          My name Mudassir Ajmal !
+        </h1>
+        <h2>
+          Reg no : 21MDSWE152
+        </h2>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
